ff891712b2deebc6608563117f3b1a5474a1fb2f
Thales Oliveira S. Costa <thalesslva67@gmail.com>
2026-01-09 22:17:09 -0300

    feat: create adress class with getters and setters

---
+++ sistema de cadastro/src/develop/desafio/cadastro/models/Adress.java
@@ -0,0 +1,31 @@
+package develop.desafio.cadastro.models;
+
+public class Adress {
+    String street;
+    String city;
+    String houseNumber;
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getHouseNumber() {
+        return houseNumber;
+    }
+
+    public void setHouseNumber(String houseNumber) {
+        this.houseNumber = houseNumber;
+    }
+}
