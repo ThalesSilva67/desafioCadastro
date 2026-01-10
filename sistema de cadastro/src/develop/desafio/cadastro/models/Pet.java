3942ee0ba2b444e656212631c2d9bbd64052002e
Thales Oliveira S. Costa <thalesslva67@gmail.com>
2026-01-09 22:16:59 -0300

    feat: create pet class with getters and setters

---
+++ sistema de cadastro/src/develop/desafio/cadastro/models/Pet.java
@@ -0,0 +1,67 @@
+package develop.desafio.cadastro.models;
+
+public class Pet {
+    String name;
+    String breed;
+    Gender gender;
+    Type type;
+    Adress adress;
+    int age;
+    double weight;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public void setType(Type type) {
+        this.type = type;
+    }
+
+    public Adress getAdress() {
+        return adress;
+    }
+
+    public void setAdress(Adress adress) {
+        this.adress = adress;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+}
